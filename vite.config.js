a35e252aeeaadeb9215e36b9a767e9ac014aae5a
khaledh150 <khaledhamamd150@gmail.com>
2025-11-25 21:16:00 +0700

    Clean repo and prepare deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  base: '/wonder-phonics/', // must match repo name
+});
